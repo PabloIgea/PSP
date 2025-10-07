553d18215bfe964dff87547c00266b86932a7939
PabloIgea <pablo.igealuengo@gmail.com>
2025-10-07 21:31:36 +0200

    Apuntes definitivos me cago en TO

--- src/edu/thepower/u1programacion/multiproceso/U1P04ContadorVocal.java
+++ src/edu/thepower/u1programacion/multiproceso/U1P04ContadorVocal.java
@@ -4,8 +4,21 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class U1P04ContadorVocal {
+
+    private static final Map <Character,Character> VOCALES;
+
+    static{
+        VOCALES = new HashMap();
+        VOCALES.put('a','á');
+        VOCALES.put('e','é');
+        VOCALES.put('i','í');
+        VOCALES.put('o','ó');
+        VOCALES.put('u','ú');
+    }
 
     private void contarVocal(char vocal, String archivo) {
 
@@ -16,9 +29,11 @@ public class U1P04ContadorVocal {
             String line;
             while ((line = in.readLine()) != null) {
 
+                line = line.toLowerCase();
+
                 for (int i = 0; i < line.length(); i++) {
 
-                    if (line.charAt(i) == vocal)
+                    if (line.charAt(i) == vocal || line.charAt(i) == VOCALES.get(vocal) )
                         contador++;
                 }
             }
@@ -40,7 +55,7 @@ public class U1P04ContadorVocal {
 
         U1P04ContadorVocal test = new U1P04ContadorVocal();
 
-        test.contarVocal('a', "./resources/vocales.txt");
+        test.contarVocal(args[0].charAt(0),args[1]);
     }
     // Que las vocales salgan de los argumentos desde eñ array de strings del main --Como con los números--
     //Crear ejecutarContadorVocales con 5 procesos
