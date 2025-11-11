f0c88d09620b69a2da8cbefe2c62381299c4409f
AlumnoAfternoon <pablo.igealuengo@gmai.com>
2025-11-11 21:17:54 +0100

    .

--- src/edu/thepower/u3comunicacionEnRed/U302EchoClient.java
+++ src/edu/thepower/u3comunicacionEnRed/U302EchoClient.java
@@ -2,8 +2,11 @@ package edu.thepower.u3comunicacionEnRed;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class U302EchoClient {
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -13,14 +16,13 @@ public class U302EchoClient {
             InputStream is = socket.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-            pw.println("ECHO SERVIDOR");
-
-            String line = null;
-            while((line = br.readLine()) != null){
-
-                System.out.println("Recibido del servidor: "+ line);
-
-            }
+            String entrada;
+            do {
+                entrada = sc.nextLine();
+                pw.println(entrada);
+                String line = null;
+                System.out.println("Recibido del servidor: "+ br.readLine());
+            }while (!entrada.equals("0"));
         } catch (IOException e) {
             System.out.println("Error al entrar al servidor");
         }
